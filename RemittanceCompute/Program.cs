3a570deb852f453ce538b629947486d18eb9e307
Safayat Borhan <SAFBO@orsted.com>
2022-11-28 12:59:01 +0800

    Add api for calculate amount.

--- RemittanceCompute/Program.cs
+++ RemittanceCompute/Program.cs
@@ -17,22 +17,3 @@ Remittance.Calculate(ref amount, ref moneyToSend, incentive);
 var totalAmount = (amount/conversionRate) + transactionCost;
 
 Console.WriteLine($"Total amount: {Math.Round(totalAmount, 2)}");
-
-public class Remittance
-{
-    public static Task Calculate(ref decimal amount, ref decimal moneyToSend, decimal incentive)
-    {       
-        var obtainedIncentive = moneyToSend * (incentive / 100);
-        var draftAmount = moneyToSend - obtainedIncentive;
-        var incentiveAmountForDraft = draftAmount * (incentive / 100);
-        moneyToSend = obtainedIncentive - incentiveAmountForDraft;
-        amount += draftAmount;
-
-        if (Math.Round(moneyToSend) != 0)
-        {
-            Calculate(ref amount, ref moneyToSend, incentive);
-        }
-
-        return Task.CompletedTask;
-    }
-}
