82a1672480214c24c726f8606503f11babcc841a
Safayat Borhan <SAFBO@orsted.com>
2022-12-29 09:58:25 +0800

    Minor changes

--- RemittanceCompute/Remittance.cs
+++ RemittanceCompute/Remittance.cs
@@ -1,20 +1,15 @@
-﻿
-
-
-
-
-
-
-public class Remittance
+﻿public class Remittance
 {
+    // Calculate the precise amount that needs to be sent and updates the reference variable "amount".
     public static Task Calculate(ref decimal amount, ref decimal moneyToSend, decimal incentive)
     {       
         var obtainedIncentive = moneyToSend * (incentive / 100);
-        var draftAmount = moneyToSend - obtainedIncentive;
-        var incentiveAmountForDraft = draftAmount * (incentive / 100);
-        moneyToSend = obtainedIncentive - incentiveAmountForDraft;
-        amount += draftAmount;
+        var leftOverAmount = moneyToSend - obtainedIncentive;
+        var incentiveAmountForLeftOver = leftOverAmount * (incentive / 100);
+        moneyToSend = obtainedIncentive - incentiveAmountForLeftOver;
+        amount += leftOverAmount; // amount is the final amount that needs to be sent.
 
+        // moneyToSend is decreasing as for each iteration we are subtracting the incentive amount. 
         if (Math.Round(moneyToSend) != 0)
         {
             Calculate(ref amount, ref moneyToSend, incentive);
