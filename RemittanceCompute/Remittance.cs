aa1ec3df3ddb8e4f542c7419ad1d7672c21c3a02
Mohammod Al Amin Ashik <mash@sitecore.net>
2023-02-04 22:53:47 +0800

    optimized to O(1)

--- RemittanceCompute/Remittance.cs
+++ RemittanceCompute/Remittance.cs
@@ -2,19 +2,8 @@
 {
     // Calculate the precise amount that needs to be sent and updates the reference variable "amount".
     public static Task Calculate(ref decimal amount, ref decimal moneyToSend, decimal incentive)
-    {       
-        var obtainedIncentive = moneyToSend * (incentive / 100);
-        var leftOverAmount = moneyToSend - obtainedIncentive;
-        var incentiveAmountForLeftOver = leftOverAmount * (incentive / 100);
-        moneyToSend = obtainedIncentive - incentiveAmountForLeftOver;
-        amount += leftOverAmount; // amount is the final amount that needs to be sent.
-
-        // moneyToSend is decreasing as for each iteration we are subtracting the incentive amount. 
-        if (Math.Round(moneyToSend) != 0)
-        {
-            Calculate(ref amount, ref moneyToSend, incentive);
-        }
-
+    {
+        amount = Math.Ceiling(moneyToSend / (1 + incentive / 100));
         return Task.CompletedTask;
     }
 }
